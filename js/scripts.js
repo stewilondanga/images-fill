f1b8f3e7ed8ec2c40cbcd87bd084a6c50642c137
Stewart ilondanga <stilondanga@gmail.com>
2019-01-16 09:57:48 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,11 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+MorphSVGPlugin.convertToPath("circle, rect");
+TweenLite.defaultEase = Power3.easeInOut;
+var dur = 2;
+var tl = new TimelineMax({yoyo:true, repeat:-1, repeatDelay:0.5});
+
+tl.to(".start", dur, {morphSVG:"#end"}, 0);
+tl.from("#kid", dur, {attr:{x:0, y:0, width:800, height:800}, opacity:0}, 0);
